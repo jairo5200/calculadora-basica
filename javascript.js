892b82085a3b3f75132ad9108ed626fc9a019a71
jairo5200 <jairo5200@gmail.com>
2015-02-25 08:30:35 -0500

    segundo commit ya completo
    se completo

--- javascript.js
+++ javascript.js
@@ -20,3 +20,25 @@ function calculadora(digito){
 function igual(){
 	document.calcular.resultado.value = eval (document.calcular.resultado.value)
 }
+
+function exponencial(){
+	document.calcular.resultado.value=document.calcular.resultado.value*document.calcular.resultado.value
+}
+
+function raiz(){
+	document.calcular.resultado.value=Math.sqrt(document.calcular.resultado.value)
+
+}
+
+function cosas(digito){
+	if(digito=='sen'){
+		document.calcular.resultado.value=Math.sin(document.calcular.resultado.value)
+	}
+	if(digito=='cos'){
+		document.calcular.resultado.value=Math.cos(document.calcular.resultado.value)
+	}
+	if(digito=='tan'){
+		document.calcular.resultado.value=Math.tan(document.calcular.resultado.value)
+	}
+	
+}
